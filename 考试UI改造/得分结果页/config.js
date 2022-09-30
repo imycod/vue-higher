6d4c587773f32a80e32a4098d50eafef527fb92a
DESKTOP-E9AIPFD\31700 <317009160@qq.com>
2022-09-30 16:21:11 +0800

    高阶组件，ui与逻辑相互分离

---
+++ 考试UI改造/得分结果页/config.js
@@ -0,0 +1,115 @@
+const columns1 = Object.freeze([
+    {
+        title: '姓名',
+        key: 'userName',
+        align: 'center'
+    },
+    {
+        title: '交卷时间',
+        key: 'submitTimeFormat',
+        align: 'center'
+    },
+    {
+        title: '用时',
+        key: 'spentTimeFormat',
+        align: 'center'
+    },
+    {
+        title: '成绩',
+        key: 'stuScore',
+        align: 'center'
+    },
+    {
+        title: '正确率',
+        key: 'correctRate',
+        align: 'center'
+    },
+    {
+        title: '得分率',
+        key: 'scoreRete',
+        align: 'center'
+    }
+])
+
+const scoreInfo1 = {
+    "topics": [],
+    "score": 100,
+    "stuScore": 0,
+    "markFlag": 1,
+    "userName": "0001",
+    "avatar": "https://test-obs.goktech.cn/202209011662023393452.gif",
+    "no": "stu00001",
+    "viewLimit": 0,
+    "objectScore": 100,
+    "subjectScore": 0,
+    "stuObjectScore": 0,
+    "stuSubjectScore": 0,
+    "testName": "测试",
+    "scoreRete": "0%",
+    "correctRate": "0%",
+    "spentTime": "7201287",
+    "submitTime": "1662612005013",
+    "remarkId": "0",
+    "remark": "",
+    "spentTimeFormat": "2小时1秒",
+    "submitTimeFormat": "2022-09-08 12:40:05"
+}
+
+
+const columns2 = Object.freeze([
+    {
+        title: '姓名',
+        key: 'userName',
+        align: 'center'
+    },
+    {
+        title: '交卷时间',
+        key: 'submitTimeFormat',
+        align: 'center'
+    },
+    {
+        title: '用时',
+        key: 'spentTimeFormat',
+        align: 'center'
+    },
+    {
+        title: '成绩',
+        key: 'stuScore',
+        align: 'center'
+    },
+    {
+        title: '正确率',
+        key: 'correctRate',
+        align: 'center'
+    },
+    {
+        title: '得分率',
+        key: 'scoreRete',
+        align: 'center'
+    }
+])
+
+
+const scoreInfo2 = {
+    "topics": [],
+    "score": 100,
+    "stuScore": 0,
+    "markFlag": 1,
+    "userName": "0001",
+    "avatar": "https://test-obs.goktech.cn/202209011662023393452.gif",
+    "no": "stu00001",
+    "viewLimit": 0,
+    "objectScore": 100,
+    "subjectScore": 0,
+    "stuObjectScore": 0,
+    "stuSubjectScore": 0,
+    "testName": "lld-测试统计",
+    "scoreRete": "0%",
+    "correctRate": "0%",
+    "spentTime": "7201287",
+    "submitTime": "1662612005013",
+    "remarkId": "0",
+    "remark": "",
+    "spentTimeFormat": "2小时1秒",
+    "submitTimeFormat": "2022-09-08 12:40:05"
+}
